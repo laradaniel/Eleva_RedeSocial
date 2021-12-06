c234b827faf75bc5e6bef07a7376af3ee18ef32a
Fiama <fividal1@hotmail.com>
2021-12-06 10:26:56 -0300

    Task 3

---
+++ elevaredesocial/src/main/java/br/org/generation/redesocial/eleva/ElevaredesocialApplication.java
@@ -0,0 +1,13 @@
+package br.org.generation.redesocial.eleva;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElevaredesocialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElevaredesocialApplication.class, args);
+	}
+
+}
