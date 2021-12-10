d5ec461de8a15a09b4442125394810fddca4ea26
Lara Daniel <laradanielt@gmail.com>
2021-12-10 10:50:22 -0300

    Criando Model Usuario e Postagem / Relacionamento entre tabelas

---
+++ elevaredesocial/src/main/java/br/org/generation/redesocial/eleva/model/Postagem.java
@@ -0,0 +1,114 @@
+package br.org.generation.redesocial.eleva.model;
+
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@Entity
+@Table(name = "tb_postagens")
+public class Postagem {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date data = new java.sql.Date(System.currentTimeMillis());
+	
+	@NotBlank(message = "O atributo Título é obrigatório")
+	@Size(min = 2, max= 200, message = "O atributo título deve conter mínimo 5 e máximo 200 caracteres")
+	private String titulo;
+	
+	@NotNull(message = "O atributo Texto é obrigatório")
+	@Size(min = 5, max = 1500, message = "O atributo texto deve conter mínimo 5 e máximo 1500 caracteres")
+	private String texto;
+	
+	private String imagem;
+	
+	private int curtir;
+	
+	@ManyToOne
+	@JsonIgnoreProperties("postagens")
+	private Tema tema;
+	
+	@ManyToOne
+	@JsonIgnoreProperties("postagens")
+	private Usuario usuario;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Date getData() {
+		return data;
+	}
+
+	public void setData(Date data) {
+		this.data = data;
+	}
+
+	public String getTitulo() {
+		return titulo;
+	}
+
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+
+	public String getTexto() {
+		return texto;
+	}
+
+	public void setTexto(String texto) {
+		this.texto = texto;
+	}
+
+	public String getImagem() {
+		return imagem;
+	}
+
+	public void setImagem(String imagem) {
+		this.imagem = imagem;
+	}
+
+	public int getCurtir() {
+		return curtir;
+	}
+
+	public void setCurtir(int curtir) {
+		this.curtir = curtir;
+	}
+
+	public Tema getTema() {
+		return tema;
+	}
+
+	public void setTema(Tema tema) {
+		this.tema = tema;
+	}
+
+	public Usuario getUsuario() {
+		return usuario;
+	}
+
+	public void setUsuario(Usuario usuario) {
+		this.usuario = usuario;
+	}
+
+}
--- elevaredesocial/src/main/java/br/org/generation/redesocial/eleva/model/Tema.java
+++ elevaredesocial/src/main/java/br/org/generation/redesocial/eleva/model/Tema.java
@@ -1,13 +1,19 @@
 package br.org.generation.redesocial.eleva.model;
 
+import java.util.List;
+
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 @Entity
 @Table(name = "tb_temas")
@@ -29,6 +35,10 @@ public class Tema {
 	@NotBlank(message = "O atributo Palavra Chave é obrigatório")
 	@Size(min = 2, max = 255, message = "O atributo possuí um mínimo de 5 caracteres e máximo de 255 caracteres")
 	private String palavraChave;
+	
+	@OneToMany(mappedBy = "tema", cascade = CascadeType.ALL)
+	@JsonIgnoreProperties("tema")
+	private List<Postagem> postagens;
 
 	public Long getId() {
 		return id;
@@ -61,10 +71,13 @@ public class Tema {
 	public void setPalavraChave(String palavraChave) {
 		this.palavraChave = palavraChave;
 	}
-	
-	
-	
-	
-	
+
+	public List<Postagem> getPostagens() {
+		return postagens;
+	}
+
+	public void setPostagens(List<Postagem> postagens) {
+		this.postagens = postagens;
+	}
 	
 }
